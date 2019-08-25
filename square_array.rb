d8d32f5e16b1b29d6c0f3cb8c1bf53d15b27bf7b
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-25 01:17:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array.each 
 end
 
-def new_square_array(num)
-  new_square_array.each do |num|
+def square_array(num)
+  square_array.each do |num|
   puts (#{num} ** 2)
 end
